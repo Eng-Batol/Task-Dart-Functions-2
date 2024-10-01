3ebe62b4549d0dfc8177a01d53b1193dff14eefd
eng-batol <batol.alashwak@gmail.com>
2024-10-01 12:04:20 +0300

    chalnge1

--- challenge1.dart
+++ challenge1.dart
@@ -1,8 +1,18 @@
+void main() {
+  printName();
+  printAge(1996); // i pass the year that i want to calculate
+  printHello("batol", "en");
+  printMax(5, 3);
+}
+
 /**
  * Task 1:
  * Create a function called `printName`
  * - that prints your name on the screen.
  */
+void printName() {
+  print("hi this is batol Al-ashwak");
+}
 
 /**
  * Task 2:
@@ -11,6 +21,11 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+void printAge(int birthYear) {
+  int curantYear = 2024;
+
+  print(curantYear - birthYear);
+}
 
 /**
  * Task 3:
@@ -22,6 +37,22 @@
  * -- fr: it should print `Bonjour NAME`
  * -- tr: it should print `Merhaba NAME`
  */
+void printHello(String name, String language) {
+  switch (language) {
+    // (the statment ) will take the desion to pass wich case
+    case "en":
+      print("Hello $name");
+    case "es":
+      print("Hola $name");
+    case "fr ":
+      print("Bonjour $name");
+    case "tr":
+      print("Merhaba $name");
+
+      break;
+    default:
+  }
+}
 
 /**
  * Task 4:
@@ -29,3 +60,10 @@
  * - that takes two numbers as parameters,
  * - and prints out the bigger number
  */
+void printMax(int num1, int num2) {
+  if (num1 > num2) {
+    print(num1);
+  } else {
+    print(num2);
+  }
+}
